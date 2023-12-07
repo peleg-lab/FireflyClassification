99a353527b0eb4d92a2e3ffa2151a5330978161c
Chantal <chantalnguyen@users.noreply.github.com>
2023-12-07 13:23:56 -0700

    Script for generating and cleaning timeseries

--- data/process_data.m
+++ data/process_data.m
@@ -1,52 +1,128 @@
-trajs = readmatrix('xyztj.csv');
-streaks = readmatrix('xyztk.csv');
-trajs = horzcat(trajs,streaks(:,end));
-%%
-num_traj = max(trajs(:,5));
-seqs = NaN*zeros(num_traj,1000);
-% masterseq = zeros(1,1000000);
-for i = 1:num_traj
-    traj = trajs(trajs(:,5)==i,4:6);
-    frame = traj(:,1);
-    traj(:,3) = traj(:,3)-min(traj(:,3))+1;
-    traj(:,1) = traj(:,1)-min(traj(:,1))+1;
-    seq = zeros(1,traj(end,1));
-    seq(traj(:,1))=1;
-    seqs(i,1:length(seq)) = seq;
-%     masterseq(frame(1):frame(end)) = masterseq(frame(1):frame(end))+seq;
-end
-% masterseq = masterseq(find(masterseq==1,1,'first'):find(masterseq==1,1,'last'));
-%% drop single-bit sequences
-to_drop = [];
-for i = 1:size(seqs,1)
-    seq = seqs(i,:);
-    if sum(~isnan(seq)) == 1
-        to_drop = [to_drop, i];
+%% Convert trajectory coordinates (xyztkj) to timeseries
+% Creates .csv file containing time series, species, date, dataset label,
+% temperature, and flash params
+
+% Import list of dataset labels
+opts = detectImportOptions('dataset_list.csv');
+opts.DataLines = 2;
+opts.VariableTypes{4} = 'char';
+datasets = readtable('dataset_list.csv',opts);
+
+dataset_folder = 'datasets/'; % directory containing datasets
+addpath(dataset_folder);
+
+fps = 30; % frame rate
+
+species = datasets.Species;
+species_label = zeros(size(species));
+for sp_i = 1:length(species)
+    switch species{sp_i}
+        case 'B. wickershamorum'
+            species_label(sp_i) = 0;
+        case 'P. bethaniensis'
+            species_label(sp_i) = 1;
+        case 'P. carolinus'
+            species_label(sp_i) = 2;
+        case 'P. forresti'
+            species_label(sp_i) = 3;
+        case 'P. frontalis'
+            species_label(sp_i) = 4;
+        case 'P. knulli'
+            species_label(sp_i) = 5;
+        case 'P. obscurellus'
+            species_label(sp_i) = 6;
     end
 end
-seqs(to_drop,:) = [];
+datasets.Species_label = species_label;
 
+all_seqs = [];
+
+% Generate cleaned timeseries from trajectories
+for dataset_i = 1:length(datasets.ID)
+    dataset_ID = datasets.ID{dataset_i};
+    dataset_path = [dataset_folder 'xyztkj_' dataset_ID(2:end) '.csv'];
+    xyztkj = readmatrix(dataset_path); % matrix containing xyz coordinates, time (seconds), streak #, trajectory #
+    num_traj = max(xyztkj(:,6)); % number of trajectories
+    seqs = NaN*zeros(num_traj,2000);
+    for traj_i = 1:num_traj
+        % extract single trajectory to convert to timeseries
+        traj = xyztkj(xyztkj(:,6)==traj_i, 4:6); % columns are time (s), streak #, trajectory #
+        traj(:,1) = round(traj(:,1)*fps); % convert time to frame
+        traj(:,1) = traj(:,1) - min(traj(:,1)) + 1; % start from 1
+        seq = zeros(1,traj(end,1));
+        seq(traj(:,1)) = 1;
+        if sum(seq) > 1 % only keep sequences longer than 1 bit
+            
+            % here we combine any flashes split by one 0 to a single flash
+            
+            % first convert the sequence into a string
+            seq_str = sprintf('%d', seq);
+            
+            % then we string match occurrences of '1 0 1'
+            split_flashes = strfind(seq_str, sprintf('%d', [1 0 1]));
+            if ~isempty(split_flashes)
+                for j = 1:length(split_flashes)
+                    seq(split_flashes(j):split_flashes(j)+2) = [1 1 1];
+                end
+            end
+            
+            % first convert the sequence into a string
+            seq_str = sprintf('%d', seq);
+            
+            % then we string match occurrences of '1 0 0 1'
+            split_flashes = strfind(seq_str, sprintf('%d', [1 0 0 1]));
+            if ~isempty(split_flashes)
+                for j = 1:length(split_flashes)
+                    seq(split_flashes(j):split_flashes(j)+3) = [1 1 1 1];
+                end
+            end
+            
+            if sum(seq) ~= length(seq) % skip single flashers
+                seqs(traj_i, 1:length(seq)) = seq;
+            end
+        end
+    end
+    seqs(find(all(isnan(seqs),2)),:) = []; % remove all-nan rows
+    seqs = [dataset_i*ones(size(seqs,1),1), seqs]; % append dataset ID to the left
+    all_seqs = [all_seqs; seqs];
+end
 
 %% write data
-fid = fopen('binary_sequences_Pknulli_Pcarolinus_Pfrontalis_Ppyralis_bw.csv', 'at');
+fid = fopen('flash_sequence_data.csv', 'at');
 % fprintf(fid, '\n' );
-for k = 1 : size(seqs,1)
-   seq = seqs(k,:);
-   seq = seq(1:(find(isnan(seq),1,'first')-1));
-   if length(seq)>1
-       fprintf(fid, '%.1f,', seq(1:end-1));
-       fprintf(fid, '%.1f\n', seq(end) );
-   else
-       fprintf(fid,'%.1f\n',seq);
-   end
-end
-fclose(fid);
-%%
-fid = fopen('flash_data_Pknulli_Pcarolinus_Pfrontalis_Ppyralis_bw.csv','at');
-% fprintf(fid, '\n' );
-
-for k = 1 : size(seqs,1)
-   fprintf(fid, '%s,', 'Bw');
-   fprintf(fid, '%d\n', 4 );
+fprintf(fid, '%s\n', 'Dataset,species,species_label,start_time,start_temp_F,num_flashes,flash_duration,ifi,timeseries');
+all_params = [];
+for k = 1 : size(all_seqs,1)
+    seq = all_seqs(k,2:end);
+    seq = seq(1:(find(~isnan(seq),1,'last')));
+    metadata = table2cell(datasets(all_seqs(k,1),1:5));
+    fprintf(fid, '%s,', metadata{1});
+    fprintf(fid, '%s,', metadata{2});
+    fprintf(fid, '%i,', metadata{5});
+    fprintf(fid, '%s,', metadata{3});
+    fprintf(fid, '%s,', metadata{4});
+    
+    % extract params: num_flashes, flash duration, inter-flash gap
+    ac = accumarray(nonzeros((cumsum(~seq)+1).*seq),1);
+    nac = nonzeros(ac); % list of lengths of consecutive ones
+    flash_length = mean(nac);
+    num_flashes = numel(nac);
+    tseq = ~seq;
+    zac = accumarray(nonzeros((cumsum(~tseq)+1).*tseq),1); % list of lengths of consecutive zeros
+    gap = mean(nonzeros(zac));
+    if isnan(gap)
+        gap = 0;
+    end
+    params = [num_flashes flash_length/fps gap/fps];
+    all_params = [all_params; params];
+    fprintf(fid, '%i,%.8f,%.8f,',params);
+    fprintf(fid,'"');
+    if length(seq)>1
+        fprintf(fid, '%.1f,', seq(1:end-1));
+        fprintf(fid, '%.1f', seq(end) );
+    else
+        fprintf(fid,'%.1f',seq);
+    end
+    fprintf(fid,'"\n');
 end
 fclose(fid);
